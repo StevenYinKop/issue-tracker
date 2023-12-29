01e85623fbb429b1a19a4d3306eacbabd9b99e37
Steven.Yin <20436656+StevenYinKop@users.noreply.github.com>
2023-12-30 01:26:36 +1100

    Fixing bug: https://github.com/radix-ui/themes/issues/109#issuecomment-1870510249

--- src/app/issue/[id]/EditIssueButton.tsx
+++ src/app/issue/[id]/EditIssueButton.tsx
@@ -1,6 +1,7 @@
+"use client";
 import { Issue } from '@prisma/client'
 import { Pencil2Icon, TrashIcon } from '@radix-ui/react-icons'
-import { Button, Flex, Text } from '@radix-ui/themes'
+import { AlertDialog, Button, Flex, Text } from '@radix-ui/themes'
 import Link from 'next/link'
 
 const EditIssueButton = ({ issue }: { issue: Issue }) => {
@@ -10,10 +11,59 @@ const EditIssueButton = ({ issue }: { issue: Issue }) => {
                 <Link href={`/issue/${issue.id}/edit`}>Edit Issue</Link>
                 <Pencil2Icon />
             </Button>
-            <Button className="cursor-pointer" color="red">
-                <Text>Remove Issue</Text>
-                <TrashIcon />
-            </Button>
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color="red">Revoke access</Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content style={{ maxWidth: 450 }}>
+                    <AlertDialog.Title>Revoke access</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        Are you sure? This application will no longer be accessible and any
+                        existing sessions will be expired.
+                    </AlertDialog.Description>
+
+                    <Flex gap="3" mt="4" justify="end">
+                        <AlertDialog.Cancel>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button variant="solid" color="red">
+                                Revoke access
+                            </Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
+
+            <AlertDialog.Root>
+                <AlertDialog.Trigger>
+                    <Button color="blue">
+                        <Text>Remove Issue</Text>
+                        <TrashIcon />
+                    </Button>
+                </AlertDialog.Trigger>
+                <AlertDialog.Content style={{ maxWidth: 450 }}>
+                    <AlertDialog.Title>Are you sure you want to delete this issue?</AlertDialog.Title>
+                    <AlertDialog.Description size="2">
+                        This action cannot be undone. This will permanently delete your account and remove your
+                        data from our servers.
+                    </AlertDialog.Description>
+                    <Flex gap="3" mt="4" justify="end">
+                        <AlertDialog.Cancel>
+                            <Button variant="soft" color="gray">
+                                Cancel
+                            </Button>
+                        </AlertDialog.Cancel>
+                        <AlertDialog.Action>
+                            <Button variant="soft" color="red">
+                                Delete
+                            </Button>
+                        </AlertDialog.Action>
+                    </Flex>
+                </AlertDialog.Content>
+            </AlertDialog.Root>
         </Flex>
     )
 }
